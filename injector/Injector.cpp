d9b47d4eaa386f2cd644b14fcdf6528a267ba8f9
Preston Yun <prestonyun32@gmail.com>
2023-10-30 17:31:46 -0700

    IsModuleLoaded check, better resource cleanup

--- injector/Injector.cpp
+++ injector/Injector.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "Injector.hpp"
 #include <windows.h>
+#include <Tlhelp32.h>
 
 auto IsProcessAlive = [](std::int32_t pid) -> bool {
     HANDLE process = OpenProcess(SYNCHRONIZE, FALSE, pid);
@@ -13,46 +14,73 @@ auto IsProcessAlive = [](std::int32_t pid) -> bool {
     return false;
 };
 
-bool Injector::Inject(std::string module_path, std::int32_t pid) noexcept
-{
-    if (IsProcessAlive(pid))
-    {
-        HMODULE hKernel32 = nullptr;
-        void* RemoteAddress = nullptr;
-        HANDLE ProcessHandle, hThread = nullptr;
-        LPTHREAD_START_ROUTINE LoadLibraryHandle = nullptr;
+bool Injector::IsModuleLoaded(std::int32_t pid, const std::string& module_name) {
+    HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, pid);
+    if (snapshot == INVALID_HANDLE_VALUE) {
+        return false;
+    }
 
-        if ((ProcessHandle = OpenProcess(PROCESS_ALL_ACCESS, false, pid)))
-        {
-            if ((hKernel32 = GetModuleHandle(L"Kernel32.dll")))
-            {
-                LoadLibraryHandle = reinterpret_cast<LPTHREAD_START_ROUTINE>(GetProcAddress(hKernel32, "LoadLibraryA"));
-                if (LoadLibraryHandle)
-                {
-                    RemoteAddress = VirtualAllocEx(ProcessHandle, nullptr, module_path.size() * sizeof(char), MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
-                    if (RemoteAddress)
-                    {
-                        if (WriteProcessMemory(ProcessHandle, RemoteAddress, &module_path[0], module_path.size() * sizeof(char), nullptr))
-                        {
-                            hThread = CreateRemoteThread(ProcessHandle, nullptr, 0, LoadLibraryHandle, RemoteAddress, 0, nullptr);
-                            if (hThread)
-                            {
-                                //More than enough time to wait for a proccess to create a thread..
-                                WaitForSingleObject(hThread, 5000);
-                                CloseHandle(hThread);
+    MODULEENTRY32 moduleEntry;
+    moduleEntry.dwSize = sizeof(MODULEENTRY32);
 
-                                VirtualFreeEx(ProcessHandle, RemoteAddress, module_path.size() * sizeof(char), MEM_RELEASE);
-                                CloseHandle(ProcessHandle);
-                                return true;
-                            }
+    // Convert std::string to std::wstring
+    std::wstring wide_module_name(module_name.begin(), module_name.end());
+
+    if (Module32First(snapshot, &moduleEntry)) {
+        do {
+            if (_wcsicmp(moduleEntry.szModule, wide_module_name.c_str()) == 0) {
+                CloseHandle(snapshot);
+                return true;
+            }
+        } while (Module32Next(snapshot, &moduleEntry));
+    }
+
+    CloseHandle(snapshot);
+    return false;
+}
+
+
+bool Injector::Inject(std::string module_path, std::int32_t pid) noexcept {
+    HMODULE hKernel32 = nullptr;
+    void* RemoteAddress = nullptr;
+    HANDLE ProcessHandle = nullptr, hThread = nullptr;
+    LPTHREAD_START_ROUTINE LoadLibraryHandle = nullptr;
+
+    if (!IsProcessAlive(pid)) {
+        return false;
+    }
+
+    if (IsModuleLoaded(pid, module_path)) {
+        return true;  // The module is already loaded; no need to inject again.
+    }
+
+    if ((ProcessHandle = OpenProcess(PROCESS_ALL_ACCESS, false, pid))) {
+        if ((hKernel32 = GetModuleHandle(L"Kernel32.dll"))) {
+            LoadLibraryHandle = reinterpret_cast<LPTHREAD_START_ROUTINE>(GetProcAddress(hKernel32, "LoadLibraryA"));
+            if (LoadLibraryHandle) {
+                RemoteAddress = VirtualAllocEx(ProcessHandle, nullptr, module_path.size() * sizeof(char), MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+                if (RemoteAddress) {
+                    if (WriteProcessMemory(ProcessHandle, RemoteAddress, &module_path[0], module_path.size() * sizeof(char), nullptr)) {
+                        hThread = CreateRemoteThread(ProcessHandle, nullptr, 0, LoadLibraryHandle, RemoteAddress, 0, nullptr);
+                        if (hThread) {
+                            WaitForSingleObject(hThread, 5000);
                         }
-
-                        VirtualFreeEx(ProcessHandle, RemoteAddress, module_path.size() * sizeof(char), MEM_RELEASE);
                     }
                 }
             }
-            CloseHandle(ProcessHandle);
         }
     }
-    return false;
+
+    // Cleanup
+    if (hThread) {
+        CloseHandle(hThread);
+    }
+    if (RemoteAddress) {
+        VirtualFreeEx(ProcessHandle, RemoteAddress, module_path.size() * sizeof(char), MEM_RELEASE);
+    }
+    if (ProcessHandle) {
+        CloseHandle(ProcessHandle);
+    }
+
+    return (hThread != nullptr);  // Return true if the thread was successfully created, false otherwise.
 }
--- injector/Injector.hpp
+++ injector/Injector.hpp
@@ -8,4 +8,5 @@ class Injector
 {
 public:
     static bool Inject(std::string module_path, std::int32_t pid) noexcept;
+    static bool IsModuleLoaded(std::int32_t pid, const std::string& module_name);
 };
